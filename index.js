99dd95bdef8c0cd6a0b459dc9662b02c1e3dc762
divya4064 <149285934+divya4064@users.noreply.github.com>
2023-11-13 19:57:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 
 const socket=require('socket.io');
 
-const server=app.listen(8080, () => {
+const server=app.listen(3000, () => {
     console.log(" Server is running on port 3000 ");
 });
 
